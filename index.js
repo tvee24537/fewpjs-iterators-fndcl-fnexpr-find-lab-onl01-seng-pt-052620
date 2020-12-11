87ad4fea224dc205ba89356221793d0df4f41b8a
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-12-11 23:53:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let year = array.find( x => x.result === "W")
+  if (year) {
+    return year.year
+  } else {
+    return undefined
+  }
+}
